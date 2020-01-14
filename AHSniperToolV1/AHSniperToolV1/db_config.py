c31f0ee09609b6fd02a673346ccf7f31504da15a
Aythriel <breaktdg@gmail.com>
2020-01-14 12:07:37 +0200

    Logical Fixes to db_config initOracleDB()

--- AHSniperToolV1/AHSniperToolV1/db_config.py
+++ AHSniperToolV1/AHSniperToolV1/db_config.py
@@ -51,74 +51,84 @@ class UndermineConn:
 #for price data on each auction it pulls from underminejournal
 
 def initOracleDB():
+    
+
     oracle = OracleConn()
     undermine = UndermineConn()
 
     orclCursor = oracle.cursor()
-
-    #check for items table
-    orclCursor.execute('SELECT * FROM Items')
-    orclExistingItems = orclCursor.fetchmany(5)
-    if (len(orclExistingItems) < 1 ) and undermine.is_connected() : 
-        print('Table Items was found uninitiated. Fetching items from underminejournal...')
-        cursorUndermine = undermine.cursor()
-        #underquery="SELECT DISTINCT(I.id),I.name_enus, G.median FROM tblDBCItem I, tblItemGlobal G WHERE I.id=G.item AND REGION='EU'"
-        underquery="SELECT I.id,I.name_enus, G.median FROM tblDBCItem I INNER JOIN tblItemGlobal G ON I.id=G.item AND REGION='US' GROUP BY I.id"
-        cursorUndermine.execute(underquery)
-        records = cursorUndermine.fetchall()
-        print ('Total items fetched from underminejournal:  ', len(records))
+    try:
+        #check for items table
+        orclCursor.execute('SELECT * FROM Items')
+        orclExistingItems = orclCursor.fetchmany(5)
+        if (len(orclExistingItems) < 1 ) and undermine.is_connected() : 
+            print('Table Items was found uninitiated. Fetching items from underminejournal...')
+            cursorUndermine = undermine.cursor()
+            #underquery="SELECT DISTINCT(I.id),I.name_enus, G.median FROM tblDBCItem I, tblItemGlobal G WHERE I.id=G.item AND REGION='EU'"
+            underquery="SELECT I.id,I.name_enus, G.median FROM tblDBCItem I INNER JOIN tblItemGlobal G ON I.id=G.item AND REGION='US' GROUP BY I.id"
+            cursorUndermine.execute(underquery)
+            records = cursorUndermine.fetchall()
+            print ('Total items fetched from underminejournal:  ', len(records))
             
-        #inserting items into oracle 
-        for record in records:
-            itemID = record[0]
-            itemName = record[1]
-            itemValue = record[2]
-            print("Inserting Item: {} - {}".format(itemID,itemName))
-            orclCursor.callproc('table_dml.insert_item', [itemID, itemName, itemValue])
-        print('Done inserting items.')
-    orclCursor.execute('SELECT * FROM Auctions')
-    orclExistingAuctions = orclCursor.fetchmany(5)
-    if (len(orclExistingAuctions) < 1):
-        print("Table Auctions was found empty. Fetching data from blizzard; this WILL take more than a few minutes.")
-        #auctions table needs to be created. this is a long process.
-        parameters = {'grant_type' : 'client_credentials',
-                    'client_id' : clientID,
-                    'client_secret' : clientSecret}
-        resToken = requests.post(tokenScope,data=parameters)
-        print(resToken.text)
-        jsonResponse =json.loads(resToken.text)
-        token = jsonResponse['access_token']
-        print("Got access token:{}".format(token))
+            #inserting items into oracle 
+            for record in records:
+                itemID = record[0]
+                itemName = record[1]
+                itemValue = record[2]
+                print("Inserting Item: {} - {}".format(itemID,itemName))
+                orclCursor.callproc('table_dml.insert_item', [itemID, itemName, itemValue])
+            print('Done inserting items.')
+        orclCursor.execute('SELECT * FROM Auctions')
+        orclExistingAuctions = orclCursor.fetchmany(5)
+        if (len(orclExistingAuctions) < 1):
+            print("Table Auctions was found empty. Fetching data from blizzard; this WILL take more than a few minutes.")
+            #auctions table needs to be created. this is a long process.
+            parameters = {'grant_type' : 'client_credentials',
+                        'client_id' : clientID,
+                        'client_secret' : clientSecret}
+            resToken = requests.post(tokenScope,data=parameters)
+            print(resToken.text)
+            jsonResponse =json.loads(resToken.text)
+            token = jsonResponse['access_token']
+            print("Got access token:{}".format(token))
     
-        print("Getting AH Dump location")
+            print("Getting AH Dump location")
 
-        parameters = {'locale' : 'en_GB',
-                  'access_token' : token}
-        url = ahScope + 'access_token=' + token + '&locale=en_GB'
-        print('Attempting get on:{}'.format(url))
-        res = requests.get(url)
+            parameters = {'locale' : 'en_GB',
+                      'access_token' : token}
+            url = ahScope + 'access_token=' + token + '&locale=en_GB'
+            print('Attempting get on:{}'.format(url))
+            res = requests.get(url)
 
-        print("Getting json... this takes more than a few minutes.")
-        jsonResponse = json.loads(res.text)
-        dumpURL = jsonResponse["files"][0]["url"]
-        dumpITSELF = requests.get(dumpURL).text    
-        print("Got the huge ass json dump.")
-        dumpOBJ = json.loads(dumpITSELF)    
-        print("Got a usable object. Begining insertion.")    
-        realm = "Silvermoon"
-        insertedItems = 0
-        for auction in dumpOBJ["auctions"] :
-            itemId = auction["item"]
-            if itemId in verbotenItems: #items that just don't belong lol.
-                continue
-            idAuction = auction["auc"]
-            #seller = auction["owner"] # blizzard doesn't provide this anymore...
-            buyout = auction["buyout"]
-            currentBid = auction["bid"]
-            timeLeft = auction["timeLeft"]
+            print("Getting json... this takes more than a few minutes.")
+            jsonResponse = json.loads(res.text)
+            dumpURL = jsonResponse["files"][0]["url"]
+            dumpITSELF = requests.get(dumpURL).text    
+            print("Got the huge ass json dump.")
+            dumpOBJ = json.loads(dumpITSELF)    
+            print("Got a usable object. Begining insertion.")    
+            realm = "Silvermoon"
+            insertedItems = 0
+            for auction in dumpOBJ["auctions"] :
+                itemId = auction["item"]
+                if itemId in verbotenItems: #items that just don't belong lol.
+                    continue
+                idAuction = auction["auc"]
+                #seller = auction["owner"] # blizzard doesn't provide this anymore...
+                buyout = auction["buyout"]
+                currentBid = auction["bid"]
+                timeLeft = auction["timeLeft"]
             
-            print('Inserting auction: {}'.format(idAuction))
+                print('Inserting auction: {}'.format(idAuction))
             
-            orclCursor.callproc('table_dml.insert_auction', [idAuction, realm, buyout, currentBid, timeLeft, itemId])
-            insertedItems += 1
-        print("Procedure done. Inserted {} auctions. Starting flask server.".format(insertedItems))
+                orclCursor.callproc('table_dml.insert_auction', [idAuction, realm, buyout, currentBid, timeLeft, itemId])
+                insertedItems += 1
+            print("Procedure done. Inserted {} auctions. Starting flask server.".format(insertedItems))
+        
+    except:
+        e = sys.exc_info()[0]
+        print("Database Error: {}".format(e.args))
+        orclCursor.callproc('TABLE_MANAGEMENT.RESET_TABLES')
+        initOracleDB()
+
+            
--- AHSniperToolV1/AHSniperToolV1/static/sql/table_dml_pkg.sql
+++ AHSniperToolV1/AHSniperToolV1/static/sql/table_dml_pkg.sql
@@ -1,7 +1,203 @@
-if opRez.getvalue() == 0:
-                return "Ok. Password changed."
-            else:
-                return "Failed to update password."
+create or replace PACKAGE TABLE_DML AUTHID CURRENT_USER AS
+    PROCEDURE INSERT_ITEM(itemID IN NUMBER, itemName IN VARCHAR2, estimatedValue IN NUMBER DEFAULT 0);
+    PROCEDURE UPDATE_ITEM_NAME(itemID IN NUMBER, newName IN VARCHAR2);
+	PROCEDURE UPDATE_ITEM_PRICE(itemID IN NUMBER, newPrice IN NUMBER);
+
+    PROCEDURE INSERT_AUCTION(auctionID IN NUMBER, realm IN VARCHAR2, buyout NUMBER, currentBid IN NUMBER, timeleft IN VARCHAR2, itemID IN NUMBER);
+    PROCEDURE DELETE_AUCTION(auctionID IN NUMBER);
+
+    PROCEDURE INSERT_RESERVED_AUCTION(userName IN VARCHAR2, auctionID IN NUMBER, dateMade IN DATE, dateExpires IN DATE, assignedID OUT NUMBER);
+    PROCEDURE DELETE_RESERVED_AUCTION(auctionID IN NUMBER, userID IN NUMBER, result OUT CHAR);
+
+    PROCEDURE INSERT_USER(username IN VARCHAR2, email IN VARCHAR2, realm IN VARCHAR2, funds IN NUMBER DEFAULT 0, pwHash IN RAW, pwSalt IN RAW, assignedID OUT NUMBER);
+    PROCEDURE DELETE_USER(userID IN NUMBER);
+    PROCEDURE UPDATE_USER_PARAM(userID IN NUMBER, paramToUpdate IN VARCHAR2, newValue IN VARCHAR2, opRez OUT NUMBER);
+    PROCEDURE UPDATE_USER_PW(userID IN NUMBER, newPassword IN RAW, opRez OUT NUMBER);
+
+    PROCEDURE INSERT_WISHLIST(user_name IN VARCHAR2, itemID IN NUMBER, assignedID OUT NUMBER);
+    PROCEDURE DELETE_WISHLIST(userID IN NUMBER, itemID in NUMBER, opRez out CHAR);
+END;
+
+create or replace PACKAGE BODY TABLE_DML IS
+
+    PROCEDURE INSERT_ITEM(itemID IN NUMBER, itemName IN VARCHAR2, estimatedValue IN NUMBER DEFAULT 0 ) IS
+    BEGIN
+        INSERT INTO ITEMS VALUES(itemID, itemName, estimatedValue);
+        COMMIT;
+        EXCEPTION WHEN OTHERS THEN
+            RAISE;
+    END INSERT_ITEM;
+
+    PROCEDURE DELETE_ITEM(itemID IN NUMBER) IS  
+    BEGIN
+        DELETE FROM ITEMS WHERE id = itemID;
+        COMMIT;
+        EXCEPTION WHEN OTHERS THEN
+            RAISE;
+    END DELETE_ITEM;
+
+    PROCEDURE UPDATE_ITEM_NAME(itemID IN NUMBER, newName IN VARCHAR2) IS
+    BEGIN
+        UPDATE ITEMS SET ITEMS.name = newName WHERE ITEMS.id = itemID;
+        COMMIT;
+        EXCEPTION WHEN OTHERS THEN
+            RAISE;
+    END UPDATE_ITEM_NAME;
+
+	PROCEDURE UPDATE_ITEM_PRICE(itemID IN NUMBER, newPrice IN NUMBER) IS
+    BEGIN
+        UPDATE ITEMS SET ITEMS.average_price = newPrice WHERE ITEMS.id = itemID;
+        COMMIT;
+        EXCEPTION WHEN OTHERS THEN
+            RAISE;
+    END UPDATE_ITEM_PRICE;
+
+    PROCEDURE INSERT_AUCTION(auctionID IN NUMBER, realm IN VARCHAR2, buyout NUMBER, currentBid IN NUMBER, timeleft IN VARCHAR2, itemID IN NUMBER) IS
+    BEGIN
+        INSERT INTO AUCTIONS(ID_AUCTION, REALM, BUYOUT_VALUE, CURRENT_BID, TIMELEFT,ID_ITEM)
+            VALUES(auctionID, realm, buyout,currentBid,timeleft,itemID);
+        COMMIT;
+        EXCEPTION WHEN OTHERS THEN
+            RAISE;
+    END INSERT_AUCTION;
+
+    PROCEDURE DELETE_AUCTION(auctionID IN NUMBER) IS
+    BEGIN
+        DELETE FROM AUCTIONS WHERE id_auction = auctionID;
+        COMMIT;
+        EXCEPTION WHEN OTHERS THEN
+            RAISE;
+    END DELETE_AUCTION;
+
+    PROCEDURE INSERT_RESERVED_AUCTION(userName IN VARCHAR2, auctionID IN NUMBER, dateMade IN DATE, dateExpires IN DATE, assignedID OUT NUMBER) IS
+        maxID NUMBER;
+        userID NUMBER;
+        alreadyExists NUMBER;
+    BEGIN
+        userID := 0;
+        alreadyExists := 0;
+        SELECT ua.ID INTO userID FROM USERACCOUNTS ua WHERE ua.username = userName;
+        IF userID <> 0 THEN
+            SELECT COUNT(*) INTO alreadyExists FROM RESERVED_AUCTIONS WHERE ID_AUCTION = auctionID;
+            IF alreadyExists = 0 THEN
+                maxID:=0;
+                SELECT MAX(ID) INTO maxID FROM RESERVED_AUCTIONS;
+                maxID := maxID + 1;
+                INSERT INTO RESERVED_AUCTIONS(ID, ID_USER, ID_AUCTION, DATE_MADE, DATE_EXPIRES) VALUES (maxID, userID, auctionID, dateMade, dateExpires);
+                assignedID := maxID;
+                COMMIT;
+            ELSE
+                assignedID := null;
+            END IF;
+        ELSE
+            assignedID :=null;
+        END IF;
+        EXCEPTION WHEN OTHERS THEN
+            assignedID:=null;
+            raise;
+    END INSERT_RESERVED_AUCTION;
+
+    PROCEDURE DELETE_RESERVED_AUCTION(auctionID IN NUMBER, userID IN NUMBER, result OUT CHAR) IS
+    BEGIN
+        DELETE FROM RESERVED_AUCTIONS WHERE id_auction=auctionID;
+        result := 'T';
+        COMMIT;
+        EXCEPTION WHEN OTHERS THEN
+            result := 'F';
+            RAISE;    
+    END DELETE_RESERVED_AUCTION;
+
+    PROCEDURE INSERT_USER(username IN VARCHAR2, email IN VARCHAR2, realm IN VARCHAR2, funds IN NUMBER DEFAULT 0, pwHash IN RAW, pwSalt IN RAW, assignedID OUT NUMBER) IS
+     maxID NUMBER;
+    BEGIN
+        maxID:=0;
+        SELECT MAX(ID) INTO maxID FROM USERACCOUNTS;
+        maxID:= maxID + 1;
+        INSERT INTO USERACCOUNTS(ID, USERNAME, EMAIL, REALM, FUNDS, PW_HASH, PW_SALT)
+            VALUES(maxID,username,email,realm,funds,pwHash,pwSalt);
+        assignedID:=maxID;
+        COMMIT;
+        EXCEPTION WHEN OTHERS THEN
+            assignedID:=null;
+            raise;
+    END INSERT_USER;
+
+    PROCEDURE DELETE_USER(userID IN NUMBER) IS
+    BEGIN
+        DELETE FROM USERACCOUNTS WHERE id=userID;   
+        COMMIT;
+        EXCEPTION WHEN OTHERS THEN
+            RAISE;
+    END DELETE_USER;
+
+    PROCEDURE UPDATE_USER_PARAM(userID IN NUMBER, paramToUpdate IN VARCHAR2, newValue IN VARCHAR2, opRez OUT NUMBER) IS
+        command VARCHAR2(200);
+    BEGIN
+        IF LOWER(paramToUpdate) IN ('username', 'email', 'realm', 'funds') THEN 
+            command := 'UPDATE USERACCOUNTS SET ' || LOWER(paramToUpdate) || '=''' || LOWER(newValue) || ''' WHERE id=' || userID;
+            dbms_output.put_line('Comanda ce se va executa:' || command);
+            EXECUTE IMMEDIATE command;
+            opRez := 0;
+            COMMIT;
+        ELSE
+            opRez := 99;
+        END IF;
+        EXCEPTION WHEN OTHERS THEN
+            opRez := 99;
+            RAISE;
+    END UPDATE_USER_PARAM;
+
+    PROCEDURE UPDATE_USER_PW(userID IN NUMBER, newPassword IN RAW, opRez OUT NUMBER) IS
+    BEGIN
+        UPDATE USERACCOUNTS SET pw_hash = newPassword WHERE id=userID;
+        opRez :=0;
+        COMMIT;
+        EXCEPTION WHEN OTHERS THEN
+            opRez :=99;
+            RAISE;
+    END UPDATE_USER_PW;
+
+    PROCEDURE INSERT_WISHLIST(user_name IN VARCHAR2, itemID IN NUMBER, assignedID OUT NUMBER) IS
+        maxID NUMBER;
+        userID NUMBER;
+        alreadyExists NUMBER;
+    BEGIN
+        userID :=0;
+        alreadyExists :=0;
+        SELECT UA.ID INTO userID FROM USERACCOUNTS UA WHERE UA.USERNAME=user_name;
+
+        IF userID <> 0 THEN
+            SELECT COUNT(*) INTO alreadyExists FROM WISHLISTS WHERE id_user = userID AND id_item = itemID;
+            IF alreadyExists = 0 THEN
+                maxID:=0;
+                SELECT MAX(ID) INTO maxID FROM WISHLISTS;
+                maxID :=maxID +1;
+                INSERT INTO WISHLISTS(ID, ID_USER, ID_ITEM, NOTIFIED_FOR_AUCTION)
+                    VALUES(maxID, userID, itemID, 0);
+                assignedID := maxID;
+                COMMIT;
+            ELSE
+                assignedID:=null;
+            END IF;
+        ELSE
+            assignedID := null;
+        END IF;
+        EXCEPTION WHEN OTHERS THEN
+            assignedID := null;
+            RAISE;
+    END INSERT_WISHLIST;
+
+    PROCEDURE DELETE_WISHLIST(userID IN NUMBER, itemID in NUMBER, opRez out CHAR) IS 
+    BEGIN
+        DELETE FROM WISHLISTS W WHERE w.id_item = itemID AND w.id_user=userID;
+        opRez := 'T';
+        COMMIT;
+        EXCEPTION WHEN OTHERS THEN
+            opRez := 'F';
+            RAISE;
+    END DELETE_WISHLIST;
+
+END TABLE_DML;
 
 create or replace TRIGGER CALCULATE_DISCOUNT
 		BEFORE INSERT ON AUCTIONS
